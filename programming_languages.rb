c57a21a846e51e6c7e1bc5c9619f31ecfbe25668
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-11-12 15:06:47 -0500

    completed programming languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
 def reformat_languages(languages)
-  # your code here
+    new_hash = {}
+    oo_languages = languages.fetch(:oo)
+    oo_languages.each do |lang, values|
+        values.each do |key, value2|
+            new_hash[lang] = {"#{key}": value2, style: [:oo]}
+            end
+        end
+    functional_languages = languages.fetch(:functional)
+    functional_languages.each do |lang, values|
+        values.each do |key, value2|
+            if new_hash.has_key?(lang)
+                new_hash[lang] = {"#{key}": value2, style: [:oo, :functional]}
+            else
+            new_hash[lang] = {"#{key}": value2, style: [:functional]}
+        end
+        end
+    end
+    new_hash
 end
+
